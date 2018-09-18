6829d7c0f8ab988114b1fa691df5d8e1f36f755c
korvi <jm.martinez@korvi.net>
2018-09-18 17:57:55 -0500

    Add project files.

---
+++ Korvi.Conekta/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korvi.Conekta
+{
+    public class Class1
+    {
+    }
+}
